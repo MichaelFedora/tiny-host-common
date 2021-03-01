c8b618c55bb5c2120515ed1b66233b9b53423e5e
MichaelFedora <mtc120@gmail.com>
2021-02-28 21:44:16 -0500

    adjust host.js script to host auth

--- scripts/host.js
+++ scripts/host.js
@@ -1,12 +1,30 @@
 const express = require('express');
 const cors = require('cors');
 const path = require('path');
+const levelup = require('levelup');
+const encode = require('encoding-down');
+const memdown = require('memdown');
+
+const { AuthDB } = require('../lib/auth/auth-db');
+const { AuthApi } = require('../lib/auth/auth-api');
+
+const db = levelup(encode(memdown(), { valueEncoding: 'json' }))
+const authDB = new AuthDB({ }, db);
+const authApi = new AuthApi({ }, authDB);
 
 const app = express();
 app.use(cors({ origin: '*', methods: 'GET' }));
+app.use(authApi.router);
+app.get('/dump', (req, res) => {
+  const data = { };
+  const stream = db.createReadStream();
+  stream.on('data', ({ key, value }) => {
+      data[key] = value;
+  }).on('close', () => res.json(data));
+});
 app.use(express.static(path.join(__dirname, '../build')));
 app.get('*', (_, res) => res.sendFile(path.join(__dirname, '../build/index.html')));
-app.listen('6970', '0.0.0.0');
+app.listen('6971', '0.0.0.0');
 
 console.log('listening on localhost:6970!');
 
